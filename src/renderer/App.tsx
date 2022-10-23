1a1ff4f0487c1c00bb07ec60f8279ee629960129
XenoPOMP <makeapexgreatagain@gmail.com>
2022-10-23 17:54:26 +0300

    allow to resize app

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,4 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './assets/components/Layout/Layout';
 import MainPage from './assets/components/pages/MainPage/MainPage';
 
 export default function App() {
