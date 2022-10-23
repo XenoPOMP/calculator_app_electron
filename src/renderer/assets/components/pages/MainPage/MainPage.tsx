2cf92006ece1dc6a50b768fcaf7a7d9ceb540bfd
XenoPOMP <makeapexgreatagain@gmail.com>
2022-10-23 18:46:27 +0300

    create number input system

--- src/renderer/assets/components/pages/MainPage/MainPage.tsx
+++ src/renderer/assets/components/pages/MainPage/MainPage.tsx
@@ -1,43 +1,172 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../Layout/Layout';
 import styles from './MainPage.module.scss';
 
 const MainPage = () => {
+  const [previousInput, setPreviousInput] = useState('');
+  const [currentInput, setCurrentInput] = useState('');
+
+  const clearInput = () => {
+    setCurrentInput('');
+    setPreviousInput('');
+  };
+
+  const addChar = (char: string | number) =>
+    setCurrentInput((e) => `${e}${char}`);
+
+  const addZero = () =>
+    currentInput[currentInput.length - 1] !== '0'
+      ? setCurrentInput((e) => `${e}0`)
+      : '';
+
+  const formatInput = () => {
+    let format = '';
+
+    for (let e = 0; e < currentInput.length; e++) {
+      if (
+        currentInput[e] == '=' ||
+        currentInput[e] == '+' ||
+        currentInput[e] == '-' ||
+        currentInput[e] == '*' ||
+        currentInput[e] == '/'
+      ) {
+        if (
+          currentInput[e - 1] == '=' ||
+          currentInput[e - 1] == '+' ||
+          currentInput[e - 1] == '-' ||
+          currentInput[e - 1] == '*' ||
+          currentInput[e - 1] == '/'
+        ) {
+          format += `0 ${currentInput[e]} `;
+        } else format += ` ${currentInput[e]} `;
+      } else format += currentInput[e];
+    }
+
+    return format;
+  };
+
+  const proceedAction = () => {
+    const stringMath = require('string-math');
+    const result = stringMath(formatInput());
+
+    setPreviousInput(`${formatInput()} =`);
+
+    setCurrentInput(`${result}`);
+  };
+
   return (
     <Layout>
       <div className={styles.page}>
         <div className={styles.numberOutput}>
-          <div className={styles.previous}>12 x 6 =</div>
-          <div className={styles.current}>72</div>
+          <div className={styles.previous}>{previousInput}</div>
+          <div className={styles.current}>{formatInput()}</div>
         </div>
 
         <div className={styles.buttons}>
           <div className={styles.buttons}>
             <div className={styles.grid}>
-              <div className={`${styles.btn} ${styles.ac}`}>AC</div>
+              <div
+                className={`${styles.btn} ${styles.ac}`}
+                onClick={() => clearInput()}
+              >
+                AC
+              </div>
               <div className={`${styles.btn} ${styles.percent}`}>%</div>
-              <div className={`${styles.btn} ${styles.operator}`}>÷</div>
+              <div
+                className={`${styles.btn} ${styles.operator}`}
+                onClick={() => addChar('/')}
+              >
+                ÷
+              </div>
 
-              <div className={`${styles.btn} ${styles.number}`}>7</div>
-              <div className={`${styles.btn} ${styles.number}`}>8</div>
-              <div className={`${styles.btn} ${styles.number}`}>9</div>
-              <div className={`${styles.btn} ${styles.operator}`}>x</div>
+              <div
+                className={`${styles.btn} ${styles.number}`}
+                onClick={() => addChar(7)}
+              >
+                7
+              </div>
+              <div
+                className={`${styles.btn} ${styles.number}`}
+                onClick={() => addChar(8)}
+              >
+                8
+              </div>
+              <div
+                className={`${styles.btn} ${styles.number}`}
+                onClick={() => addChar(9)}
+              >
+                9
+              </div>
+              <div
+                className={`${styles.btn} ${styles.operator}`}
+                onClick={() => addChar('*')}
+              >
+                x
+              </div>
 
-              <div className={`${styles.btn} ${styles.number}`}>4</div>
-              <div className={`${styles.btn} ${styles.number}`}>5</div>
-              <div className={`${styles.btn} ${styles.number}`}>6</div>
-              <div className={`${styles.btn} ${styles.operator}`}>-</div>
+              <div
+                className={`${styles.btn} ${styles.number}`}
+                onClick={() => addChar(4)}
+              >
+                4
+              </div>
+              <div
+                className={`${styles.btn} ${styles.number}`}
+                onClick={() => addChar(5)}
+              >
+                5
+              </div>
+              <div
+                className={`${styles.btn} ${styles.number}`}
+                onClick={() => addChar(6)}
+              >
+                6
+              </div>
+              <div
+                className={`${styles.btn} ${styles.operator}`}
+                onClick={() => addChar('-')}
+              >
+                -
+              </div>
 
-              <div className={`${styles.btn} ${styles.number}`}>1</div>
-              <div className={`${styles.btn} ${styles.number}`}>2</div>
-              <div className={`${styles.btn} ${styles.number}`}>3</div>
-              <div className={`${styles.btn} ${styles.operator}`}>+</div>
+              <div
+                className={`${styles.btn} ${styles.number}`}
+                onClick={() => addChar(1)}
+              >
+                1
+              </div>
+              <div
+                className={`${styles.btn} ${styles.number}`}
+                onClick={() => addChar(2)}
+              >
+                2
+              </div>
+              <div
+                className={`${styles.btn} ${styles.number}`}
+                onClick={() => addChar(3)}
+              >
+                3
+              </div>
+              <div
+                className={`${styles.btn} ${styles.operator}`}
+                onClick={() => addChar('+')}
+              >
+                +
+              </div>
 
-              <div className={`${styles.btn} ${styles.number} ${styles.zero}`}>
+              <div
+                className={`${styles.btn} ${styles.number} ${styles.zero}`}
+                onClick={() => addChar(0)}
+              >
                 0
               </div>
               <div className={`${styles.btn} ${styles.number}`}>,</div>
-              <div className={`${styles.btn} ${styles.operator}`}>=</div>
+              <div
+                className={`${styles.btn} ${styles.operator}`}
+                onClick={() => proceedAction()}
+              >
+                =
+              </div>
             </div>
           </div>
         </div>
